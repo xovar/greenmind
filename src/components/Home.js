9649ad6abbc5d86b89f15c42489b6aad27508140
farhanxovar <90099476+farhanxovar@users.noreply.github.com>
2023-06-06 22:13:58 +0600

    hero section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,19 @@ function Home() {
   return (
     <>
         <div className='container h-[512px] flex bg-primary rounded-[24px]'>
-            <div  className='w-[497px]'>
-                <div>kldkdlklk</div>
+            <div  className='w-[497px] mt-[48px] ml-[48px]'>
+                <h1 className='w-[512px] font-poppins font-extrabold leading-[64px] text-[64px]'>Buy your dream plants</h1>
+                <div className='flex w-[318px] justify-between mt-[24px]'>
+                    <div>
+                        <p className='font-poppins font-medium text-[32px]'>50+</p>
+                        <p className='font-poppins font-medium text-[18px]'>Plant Species</p>
+                    </div>
+                    <div className='border-r-[1px] h-[64px] border-solid border-black'></div>
+                    <div>
+                        <p className='font-poppins font-medium text-[32px]'>100+</p>
+                        <p className='font-poppins font-medium text-[18px]'>Customers</p>
+                    </div>
+                </div>
             </div>
             <div  className='w-[747px] relative'>
                 <img src="./img/arrow-1.png" className='absolute top-[319.91px]' alt="" />
